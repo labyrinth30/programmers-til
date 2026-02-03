6a2db10f3dc5c3357e6e8c1b10864275b651352f
younha00 <ayeon0@office.skhu.ac.kr>
2026-02-03 16:30:45 +0900

    Feat: 0204 추가

--- week5/project/routes/channels.js
+++ week5/project/routes/channels.js
@@ -1,26 +1,49 @@
 import express from 'express';
 const router = express.Router();
 router.use(express.json());
+import {body, param, validationResult} from 'express-validator';
 import conn from '../db.js';
+
+const validate = (req, res, next) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        next();
+    }
+    return res.status(400).json({"message": errors.array()[0].msg});
+}
 
 router
     .route('/')
-    .get((req, res) => {
+    .get([
+        body('userId').notEmpty().isInt().withMessage('userId must be a number'),
+        validate
+        ],
+        (req, res) => {
         let { userId } = req.body;
         let sql = 'SELECT * FROM channels WHERE user_id = ?'
-
-        // && 연산자로 userId 유효성 검사
-        userId && conn.query(sql, userId,
+        conn.query(sql, userId,
             (err, results) => {
-            if(results.length) res.json(results);
-            else res.status(404).json({"message": "No Channel Found"});
+                if (err) {
+                    console.error("Database Error:", err);
+                    return res.status(500).json({
+                        "message": "서버 내부 오류가 발생했습니다. 잠시 후 다시 시도해주세요."
+                    });
+                }
+            if (results.length) return res.json(results);
+            else return res.status(404).json({"message": "No Channel Found"});
             })
     })
-    .post((req, res) => {
+    .post(body('userId').notEmpty().isInt().withMessage('userId must be a number'),
+        body('name').notEmpty().isString().withMessage('name must be a string')
+        ,(req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) res.status(400).json({"message": errors.array()[0].msg});
+
         const { name, userId } = req.body;
-        if (!name || !userId) res.status(400).json({"message": "name or userId is required"});
+
         let sql = `INSERT INTO channels (name, user_id) VALUES (?, ?)`;
         let values = [name, userId];
+
         conn.query(sql, values, (err, results) => {
             if (err) return res.status(500).json(err);
             res.status(201).json(results);
@@ -42,26 +65,51 @@ router
             }
         )
     })
-    .patch((req, res) => {
+    .put(
+        param('id').notEmpty().withMessage('채널id가 필요해요.').isInt().withMessage('id must be a number'),
+        body('name').notEmpty().isString().withMessage('name must be a string'),
+        (req, res) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) res.status(400).json({"message": errors.array()[0].msg});
         const id = parseInt(req.params.id);
-        let channel = db.get(id);
-        if (!channel) return res.status(404).json({"message": `${id} Channel Not Found`});
-        const oldChannelTitle = channel.channelTitle;
-        const newChannel = {
-            ...channel,
-            ...req.body
-        };
-        db.set(id, newChannel);
-        res.json({
-            "message": `${oldChannelTitle}이 ${newChannel.channelTitle}로 변경되었습니다.`,
-        })
+        const { name } = req.body;
+        const sql = 'UPDATE channels SET name=? WHERE id = ?'
+        const values = [name,id];
+
+        conn.query(sql, values,
+            (err, results) => {
+                if (err) {
+                    console.error("Database Error:", err);
+                    return res.status(500).json({
+                        "message": "서버 내부 오류가 발생했습니다. 잠시 후 다시 시도해주세요."
+                    });
+                }
+                if (results.affectedRows === 0) {
+                    return res.status(404).json({"message": `${id} Channel Not Found`});
+                }
+                return res.json({
+                    "message": "Channel Updated Successfully",
+                });
+            })
     })
-    .delete((req, res) => {
+    .delete(
+        param('id').notEmpty().isInt().withMessage('id must be a number'),
+        (req, res) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({"message": errors.array()[0].msg});
+            }
         const id = parseInt(req.params.id);
-        const channel = db.get(id);
-        if (!channel) return res.status(404).json({ "message": `${id} Channel Not Found`});
-        db.delete(id);
-        res.status(204).end();
+        const sql = 'DELETE FROM channels WHERE id = ?';
+        conn.query(sql, id, (err, results) => {
+            if (err) {
+                return res.status(400).end();
+            }
+            if (results.affectedRows === 0) {
+                return res.status(404).end();
+            }
+            return res.status(204).end();
+        })
     })
 
 export default router;
