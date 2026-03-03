c339bc8a0507445238d8c229a666833f83b53aa3
younha00 <ayeon0@office.skhu.ac.kr>
2026-03-03 12:44:19 +0900

    feat: 0227 추가

--- week7/project/controllers/OrdersController.js
+++ week7/project/controllers/OrdersController.js
@@ -2,32 +2,36 @@ import conn from '../db.js';
 import statusCode from 'http-status-codes';
 
 // 주문 하기
-export const order = (req, res) => {
+export const order = async (req, res) => {
     const { items, delivery, totalQuantity, totalPrice, userId, firstBookTitle } = req.body;
 
-    let delivery_id=3;
-    let order_id=2;
+    let delivery_id;
+    let order_id;
 
     let sql = `INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?)`;
     let values = [delivery.address, delivery.receiver, delivery.contact];
-    conn.query(sql, values, (err, results) => {
+
+    const [results] = await conn.query(sql, values, (err, results) => {
         if (err) return res.status(statusCode.INTERNAL_SERVER_ERROR).json({message: err.message});
         delivery_id = results.insertId;
-        return res.status(statusCode.CREATED).json(results);
     });
+
     sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id)
             VALUES (?, ?, ?, ?, ?);`
     values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
+
     conn.query(sql, values, (err, results) => {
         if (err) return res.status(statusCode.INTERNAL_SERVER_ERROR).json({message: err.message});
         order_id = results.insertId;
-        return res.status(statusCode.CREATED).json(results);
     });
+
     sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?`;
     values = [];
+
     items.forEach((item) =>
         values.push([order_id, item.book_id, item.quantity])
     );
+
     conn.query(sql, [values], (err, results) => {
         if (err) return res.status(statusCode.INTERNAL_SERVER_ERROR).json({message: err.message});
         order_id = results.insertId;
--- week7/project/db.js
+++ week7/project/db.js
@@ -1,12 +1,15 @@
-import mariadb from 'mysql2';
+import mariadb from 'mysql2/promise';
 
 // connection 생성
-const connection = mariadb.createConnection({
-    host: 'localhost',
-    user: 'user',
-    password: 'root',
-    database: 'mydb',
-    dateStrings: true
-});
+const connection = async () => {
+    const conn = await mariadb.createConnection({
+        host: 'localhost',
+        user: 'user',
+        password: 'root',
+        database: 'mydb',
+        dateStrings: true
+    });
+return conn;
+}
 
 export default connection;
